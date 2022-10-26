4e41c0118a478123546e80067bd16fc505020f98
Pabricio Freitas <pabricio2000@gmail.com>
2022-10-26 16:45:43 -0300

    add comments, e adicionando tamanho fixo no windows

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tela_login/pages/widgets/sign_in.dart';
-import 'package:tela_login/pages/widgets/sign_up.dart';
 
 import '../theme.dart';
 import '../utils/bubble_indicator_painter.dart';
@@ -36,53 +34,66 @@ class _AuthPageState extends State<AuthPage>
     final size = MediaQuery.of(context).size;
     return Scaffold(
         body: SingleChildScrollView(
-      physics: const ClampingScrollPhysics(),
+      physics:
+          const ClampingScrollPhysics(), //impede que scroll role mais do que precisa
+      //detecta quando é clicado
       child: GestureDetector(
+        //quando clicar em qualquer parte do app
         onTap: () {
+          //o foco do teclado será tirado
           FocusScope.of(context).requestFocus(FocusNode());
         },
         child: Container(
           width: size.width,
           height: size.height,
           decoration: const BoxDecoration(
+            //criar o gradiente
             gradient: LinearGradient(
-                colors: <Color>[
-                  CustomTheme.loginGradientStart,
-                  CustomTheme.loginGradientEnd
-                ],
-                begin: FractionalOffset(0.0, 0.0),
-                end: FractionalOffset(1.0, 1.0),
-                stops: <double>[0.0, 1.0],
-                tileMode: TileMode.clamp),
+              colors: <Color>[
+                CustomTheme.loginGradientStart,
+                CustomTheme.loginGradientEnd
+              ],
+              begin: FractionalOffset(
+                  0.0, 0.0), //como as cores serão deslocada no começo
+              end: FractionalOffset(
+                  1.0, 1.0), //como as cores serão deslocada no fim
+              stops: <double>[0.0, 1.0], //criar gradiente linear
+
+              tileMode:
+                  TileMode.clamp, //O modo que os gradientes irá se comportar
+            ),
           ),
           child: Column(
+            //mando a coluna tomar o maximo de tamanho possivel
             mainAxisSize: MainAxisSize.max,
             children: <Widget>[
               Padding(
                 padding: const EdgeInsets.only(top: 20.0),
+                //crio a imagem
                 child: Image(
                     height:
-                        MediaQuery.of(context).size.height > 800 ? 191.0 : 150,
+                        size.height > 800 ? 191.0 : 150, //#determino o tamanho
                     fit: BoxFit.fill,
                     image: const AssetImage('assets/img/login_logo.png')),
               ),
               Padding(
                 padding: const EdgeInsets.only(top: 20.0),
-                child: _buildMenuBar(context),
+                child: _buildMenuBar(context), //crio o botão de ir para nova
               ),
               Expanded(
                 flex: 2,
                 child: PageView(
                   controller: _pageController,
                   physics: const ClampingScrollPhysics(),
-                  onPageChanged: (int i) {
+                  onPageChanged: (int index) {
+                    //remover o foco dos textformfields quando troca de tela
                     FocusScope.of(context).requestFocus(FocusNode());
-                    if (i == 0) {
+                    if (index == 0) {
                       setState(() {
                         right = Colors.white;
                         left = Colors.black;
                       });
-                    } else if (i == 1) {
+                    } else if (index == 1) {
                       setState(() {
                         right = Colors.black;
                         left = Colors.white;
@@ -90,14 +101,19 @@ class _AuthPageState extends State<AuthPage>
                     }
                   },
                   children: <Widget>[
-                    ConstrainedBox(
-                      constraints: const BoxConstraints.expand(),
-                      child: SignIn(),
-                    ),
-                    ConstrainedBox(
-                      constraints: const BoxConstraints.expand(),
-                      child: SignUp(),
-                    ),
+                    Container(),
+                    Container()
+                    // //tela de login
+                    // ConstrainedBox(
+                    //   constraints: const BoxConstraints.expand(),
+                    //   child: SignIn(),
+                    // ),
+                    // //tela de cadastro
+
+                    // ConstrainedBox(
+                    //   constraints: const BoxConstraints.expand(),
+                    //   child: SignUp(),
+                    // ),
                   ],
                 ),
               ),
@@ -116,23 +132,30 @@ class _AuthPageState extends State<AuthPage>
         color: Color(0x552B2B2B),
         borderRadius: BorderRadius.all(Radius.circular(25.0)),
       ),
+      //serve para desenhar coisas animadas na tela
       child: CustomPaint(
+        //Esse componente que criei ele vai controlar animação da parte branca
         painter: BubbleIndicatorPainter(pageController: _pageController),
+        //EU crio uma linha horizontal com seus filhos
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment
+              .spaceEvenly, //Aqui ele vai colocar um espaço padrão entre as linhas usando máximo de espaço
           children: <Widget>[
             Expanded(
               child: TextButton(
                 style: ButtonStyle(
+                  //determinado a cor ao aperta e segurar o botão
                   overlayColor: MaterialStateProperty.all(Colors.transparent),
                 ),
+                //se botão for pressionado ele chama a função dizendo que botão de existente clicou
                 onPressed: _onSignInButtonPress,
                 child: Text(
                   'Existing',
                   style: TextStyle(
-                      color: left,
-                      fontSize: 16.0,
-                      fontFamily: 'WorkSansSemiBold'),
+                    color: left,
+                    fontSize: 16.0,
+                    fontFamily: 'WorkSansSemiBold',
+                  ),
                 ),
               ),
             ),
